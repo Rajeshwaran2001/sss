b52e336aa8d1f09b254dc34a2e159e8e37cb6388
Rajeshwaran <54212165+Rajeshwaran2001@users.noreply.github.com>
2023-05-29 21:02:18 +0530

    initial templates and static files

---
+++ dash/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'dashboard'
+
+urlpatterns = [
+    path('home', views.home, name='home'),
+    path('change_password', views.change_password, name='change'),
+    path('logout', views.logout, name='logout')
+]
--- dash/views.py
+++ dash/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import PasswordChangeForm
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
+def home(request):
+    return render(request, 'index.html')
+
+
+@login_required()
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(user=request.user, data=request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important to maintain user session
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('Dashboard:dashboard')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(user=request.user)
+    context = {'form': form}
+    return render(request, 'change_password.html', context)
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('Login')
