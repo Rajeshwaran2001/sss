5028e7fb1d8aa5e662693988773aad4f897041d0
Rajeshwaran <54212165+Rajeshwaran2001@users.noreply.github.com>
2023-06-02 23:23:32 +0530

    service model added

--- company/admin.py
+++ company/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import issue_list, status_list, service
 
 # Register your models here.
+
+admin.site.register(issue_list)
+admin.site.register(status_list)
+admin.site.register(service)
--- company/models.py
+++ company/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from user.models import branch_user
+from utility.models import device_list
+
 
 # Create your models here.
+class issue_list(models.Model):
+    name = models.CharField(max_length=100, null=True, blank=True)
+    is_Active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+
+class status_list(models.Model):
+    name = models.CharField(max_length=100, null=True, blank=True)
+    is_Active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+
+class service(models.Model):
+    user = models.ForeignKey(branch_user, on_delete=models.SET_NULL, blank=False, null=True)
+    issue = models.ForeignKey(issue_list, on_delete=models.SET_NULL, blank=False, null=True)
+    pc = models.ForeignKey(device_list, on_delete=models.SET_NULL, blank=False, null=True)
+    status = models.ForeignKey(status_list, on_delete=models.SET_NULL, blank=False, null=True)
+    resolution = models.CharField(max_length=1000, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.issue)
