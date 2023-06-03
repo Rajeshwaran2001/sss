008fe100468dd17db7e6043d6627265bed3df279
Rajeshwaran <54212165+Rajeshwaran2001@users.noreply.github.com>
2023-06-03 17:29:30 +0530

    initial customer login page

--- sss/views.py
+++ sss/views.py
@@ -1,10 +1,11 @@
+from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.http import JsonResponse
 from django.shortcuts import render
 from dash.models import Admin
 from django.contrib.auth import logout
 from django.shortcuts import redirect
-
+from utility.models import device_list, branch
 from sss.settings import LOGIN_REDIRECT_URL
 
 
@@ -46,3 +47,18 @@ def afterlogin_view(request):
             return redirect('error')
     else:
         return redirect('Login')
+
+
+def customer_view(request):
+    br = branch.objects.all().filter(is_active=True)
+    if request.method == 'POST':
+        ad_name = request.POST.get('ad_name')
+        ad_name = ad_name.strip()  # Remove any leading or trailing spaces
+        ad_name_upper = ad_name.upper()  # Convert ad_name to uppercase
+        try:
+            ad = device_list.objects.get(AdUsername=ad_name_upper)
+        except device_list.DoesNotExist:
+            messages.error(request, f"Ad with name {ad_name_upper} does not exist.")
+            print('not fount')
+            return redirect('customer-view')
+    return render(request,'customer/login.html', {'br': br})
